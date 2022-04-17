63cf5920d40b209f953f97e57c94bafb6e6260ae
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-17 19:44:35 -0300

    feat: created colors theme Chakra

---
+++ styles/themeChakra.ts
@@ -0,0 +1,15 @@
+export const colors = {
+    primaryColor: {
+        800:"#93c2c2",
+        700:"#aadae9",
+        600:"#93d1e4",
+        500:"#d6eef5",
+        400:"#e5f4f9",
+        300:"#edf8fb"
+    },
+    text: {
+        'white': "f5f5f5",
+        'secondary': '#0c6a6b',
+        'tertiary':'#2d3a3e'
+    }
+}

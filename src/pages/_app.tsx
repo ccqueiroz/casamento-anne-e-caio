90478179472e9607260ee7f8c2d9b86d009999dd
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-17 19:44:14 -0300

    feat: created file _app with MyApp component

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import {AppProps} from 'next/app';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import '../../styles/globals.css'
+import {colors} from '../../styles/themeChakra'
+
+const theme = extendTheme({colors})
+function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
+    return (
+        <ChakraProvider theme={theme}>
+            <Component {...pageProps}/>
+        </ChakraProvider>
+
+  );
+}
+
+export default MyApp

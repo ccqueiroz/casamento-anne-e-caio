21e026f17bfa100f416ff4a0cf6b6a8d1a960ea8
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-18 21:25:28 -0300

    refactor: refactor components

--- src/components/FrontDesk/index.tsx
+++ src/components/FrontDesk/index.tsx
@@ -1,10 +1,19 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
+import { BuffetPresentation } from './BuffetPresentation';
+import { HeaderFrontDesk } from './HeaderFrontDesk';
 
 const FrontDesk: React.FC = () => {
 
     return (
-        <Box></Box>
+        <Box
+            width="100vw"
+            height="auto"
+            padding="1% 5% 1%"
+            >
+            <HeaderFrontDesk />
+            <BuffetPresentation/>
+        </Box>
     );
 }
 

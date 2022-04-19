3804ac8fe7a75de9d1673003d92a26d36b2d3da7
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-18 21:23:07 -0300

    refactor: change component in file to HeaderDividerComponent

--- src/components/CountDownSection/HeaderCountDown/index.tsx
+++ src/components/CountDownSection/HeaderCountDown/index.tsx
@@ -1,31 +1,10 @@
-import { Box, Divider, Text } from '@chakra-ui/react';
 import React from 'react';
+import { HeaderDividerComponent } from '../../HeaderDividerComponent';
 
 const HeaderCountDown: React.FC = () => {
     
     return (
-        <Box
-            width="100%%"
-            maxWidth="1200px"
-            margin="0 auto"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Divider width="35%" display={{ base: 'none', lg:'inline-block'}} bg="text.secondary" height="1px"/>
-            <Text fontSize={{
-                base: '1.25rem',
-                lg:'1.725rem'
-            }}
-            color="text.secondary"
-                fontWeight="bold"
-                flexShrink={1}
-                textAlign="center"
-            >
-                CONTAGEM REGRESSIVA
-            </Text>
-            <Divider width="35%" display={{ base: 'none', lg:'inline-block'}} bg="text.secondary" height="1px"/>
-        </Box>
+        <HeaderDividerComponent text="contagem regressiva" />
     );
 }
 

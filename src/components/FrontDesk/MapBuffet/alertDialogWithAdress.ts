9b033a31798c7ca629470b49af90fbcc9ec854a2
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-19 01:27:30 -0300

    feat: created dialog to show in Map

---
+++ src/components/FrontDesk/MapBuffet/alertDialogWithAdress.ts
@@ -0,0 +1,16 @@
+export const alertDialogWithAdress =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading"><b>Atlantis Buffet</b></h1>' +
+    "<br/>"+
+    '<div id="bodyContent">' +
+    "<p>Atlantis Buffet, Espaço em frente ao mar, " +
+    "acesso à Praia de Iparana, com lindo gramado e ambiente arborizado. </p>" +
+    "<br/>"+
+    "<hr/>" +
+    "<p><b>Endereço: R. Otaviano Laurindo, 580 - Parque Leblon, Caucaia - CE, 61631-290<b></p>" +
+    "<p><b>Telefone: 085 9 985551421</b></p>" +
+    "<b>Instagram: <a href='http://espacoatlantis.com.br/' target='_blank' >http://espacoatlantis.com.br/</a></b>" +
+    "</div>" +
+    "</div>";

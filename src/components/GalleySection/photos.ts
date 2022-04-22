880d40a006eb2a8ac00bdf94484764aef50d6572
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-22 20:14:02 -0300

    refactor: reactor params photos reductions

--- src/components/GalleySection/photos.ts
+++ src/components/GalleySection/photos.ts
@@ -3,10 +3,7 @@ import { Photo } from "../../data/model/Gallery";
 export const photos: Array<Photo> = [
     {
         src: "/images/casal6.JPG",
-        width: {
-            base: '100%',
-            lg:'100%'
-        },
+        width: "100%",
         height: {
             base: '250px',
             md:"300px",
@@ -15,18 +12,12 @@ export const photos: Array<Photo> = [
         col: {
             lg: 6
         },
-            objectFit:{
-                base: 'cover',
-                lg: 'cover'
-        },
+        objectFit:"cover",
         alt:"Anne e Caio com Puck"
     },
     {
         src: "/images/IMG_6604.JPG",
-        width: {
-            base: '100%',
-            lg:'100%'
-        },
+        width: "100%",
         height: {
             base: '250px',
             md:"300px",
@@ -35,10 +26,7 @@ export const photos: Array<Photo> = [
         col: {
             lg: 3
         },
-        objectFit:{
-            base: 'cover',
-            lg: 'cover'
-        },
+        objectFit:"cover",
         objectPosition: {
             md:"50% 40%"
         },
@@ -46,10 +34,7 @@ export const photos: Array<Photo> = [
     },
     {
         src: "/images/IMG_6602.JPG",
-        width: {
-            base: '100%',
-            lg:'435px'
-        },
+        width: "100%",
         height: {
             base: '250px',
             md:"300px",
@@ -58,10 +43,7 @@ export const photos: Array<Photo> = [
         col: {
             lg: 3
         },
-        objectFit:{
-            base: 'cover',
-            lg: 'cover'
-        },
+        objectFit:"cover",
         alt:"Anne e Caio casamento civil"
     },
     {
@@ -77,22 +59,17 @@ export const photos: Array<Photo> = [
         col: {
             lg: 2
         },
-        objectFit:{
-            base: 'cover',
-            lg: 'cover'
-        },
+        objectFit:"cover",
         alt:"Anne e Caio, foto do casamento civil"
     },
     {
         src: "/images/casal4.JPG",
-        width: {
-            base: '100%',
-            lg:'100%'
-        },
+        width: "100%",
         height: {
             base: '250px',
             lg:'435px'
-        },        col: {
+        },
+        col: {
             lg: 7
         },
         objectFit:{
@@ -108,10 +85,7 @@ export const photos: Array<Photo> = [
 
     {
         src: "/images/IMG_6596.JPG",
-        width: {
-            base: '100%',
-            lg:'435px'
-        },
+        width: "100%",
         height: {
             base: '250px',
             lg:'435px'

38d14f197be1da7e9dff8373863b5339670840f1
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-22 10:32:46 -0300

    refactor: refactor Omit children in TextProps

--- src/components/TextComponent/index.tsx
+++ src/components/TextComponent/index.tsx
@@ -1,7 +1,7 @@
 import { Text, TextProps } from '@chakra-ui/react';
 import React from 'react';
 
-interface TextComponentProps extends TextProps {
+interface TextComponentProps extends Omit<TextProps, 'children'> {
     text: string,
     textIndent?: number
 }

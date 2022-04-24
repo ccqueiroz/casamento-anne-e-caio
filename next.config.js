b95381317243b9df404350e0a19dda6e4211c6e9
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-24 01:59:50 -0300

    refactor: add env in nextConfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    FAUNA_ADMIN_KEY: process.env.FAUNA_ADMIN_KEY
+  }
 }
 
 module.exports = nextConfig

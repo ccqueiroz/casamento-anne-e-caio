7bbbbc0613e225881e4605a804447ec3a7f7af3b
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-25 01:36:18 -0300

    refactor: created GuestsFaunaDB

--- src/data/model/User/index.ts
+++ src/data/model/User/index.ts
@@ -1,9 +1,18 @@
 import { FileWithPreview } from "../Files";
 
 export interface UserModel {
+    id?: string,
     name?: string,
     email?: string,
     phone?: string,
     presenceAtTheEvent?: string
     vaccineCard?: FileWithPreview
+}
+
+export type GuestsFaunaDB = {
+    ref: {
+        id: string,
+    };
+    ts: number | string;
+    data: UserModel
 }

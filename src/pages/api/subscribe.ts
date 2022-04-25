495a93f1d74191bfc40b0b2aee98602752b51b1a
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-25 02:00:07 -0300

    refactor: customizer controllerSubscrible

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,38 +1,23 @@
 import {NextApiRequest , NextApiResponse} from 'next';
+import { handleErrors } from '../../api/errors/handleErrors';
+import { GuestsRepository } from '../../api/repositories/guestsRepository';
 
 const controllerSubscrible =  async (request: NextApiRequest, response: NextApiResponse) =>  {
-
-    // if(request.method === 'POST'){
-    //     try{
-    //         const {session:{user}} = request.body;
-    //         const customerRepository = new CustomerRepository();
-    //         const stripeCustomer = await new CreateOrListCustomers(
-    //             stripe,
-    //             user,
-    //             customerRepository
-    //         ).execute();
-    //         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-    //             customer: stripeCustomer.id,
-    //             payment_method_types: ['card'],
-    //             billing_address_collection: 'required',
-    //             line_items: [{
-    //                 price: process.env.STRIPE_PRICE_API_KEY,
-    //                 quantity:1
-    //             }],
-    //             mode: 'subscription',
-    //             allow_promotion_codes: true,
-    //             success_url:process.env.STRIPE_SUCCESS_URL,
-    //             cancel_url: process.env.STRIPE_CANCEL_URL
-    //         });
-    //         return response.status(200).json({sessionId: stripeCheckoutSession.id});
-    //     }catch(error){
-    //         const err = handleErrors(error);
-    //         return response.status(error.statusCode ?? 500).json({data: err.description ?? err.message});
-    //     }
-    // }else{
-    //     response.setHeader('allow', 'POST');
-    //     response.status(405).end('Method not allowed');
-    // }
+    if(request.method === 'PUT'){
+        try{
+            const { session:{ user } } = request.body;
+            const customerRepository = new GuestsRepository();
+            
+            
+            return response.status(200).json({});
+        }catch(error){
+            const err = handleErrors(error);
+            return response.status(error.statusCode ?? 500).json({data: err.description ?? err.message});
+        }
+    }else{
+        response.setHeader('allow', 'PUT');
+        response.status(405).end('Method not allowed');
+    }
 }
 
 export default controllerSubscrible;

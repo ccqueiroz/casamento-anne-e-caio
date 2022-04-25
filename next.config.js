793ccaef02e5a1a408e928b4360578db08482abd
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-25 02:00:34 -0300

    refactor: includes NEXT_PUBLIC_BASE_URL

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     FAUNA_ADMIN_KEY: process.env.FAUNA_ADMIN_KEY,
-    NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY
+    NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL
   }
 }
 

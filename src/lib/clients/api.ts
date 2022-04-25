5b86108768c8b06c61b72d93a93bf1c41bc91714
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-25 02:01:23 -0300

    refactor: add typign api

--- src/lib/clients/api.ts
+++ src/lib/clients/api.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
-const api = axios.create({
+const api: AxiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL
 });
 

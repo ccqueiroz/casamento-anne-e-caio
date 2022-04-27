c57c376eb79ab068f6a431b5994c0768af4f653f
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-26 21:30:08 -0300

    refactor: implementation logic controller with middleware

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,23 +1,39 @@
-import {NextApiRequest , NextApiResponse} from 'next';
+import { NextApiRequestModels, NextApiResponseModels } from '../../api/client/models/nextHTTPModels';
 import { handleErrors } from '../../api/errors/handleErrors';
 import { GuestsRepository } from '../../api/repositories/guestsRepository';
+import { SubscribeService } from '../../api/services/SubscribeService';
+import nextConnect from 'next-connect';
+import parseMultiPartyForm from '../../api/middlewares/multipartyFormMiddleware';
 
-const controllerSubscrible =  async (request: NextApiRequest, response: NextApiResponse) =>  {
-    if(request.method === 'PUT'){
-        try{
-            const { session:{ user } } = request.body;
-            const customerRepository = new GuestsRepository();
-            
-            
-            return response.status(200).json({});
-        }catch(error){
+const controllerSubscrible = nextConnect();
+
+controllerSubscrible
+    .use(parseMultiPartyForm)
+    .put(async (request: NextApiRequestModels, response: NextApiResponseModels) => {
+    if (request.method === 'PUT') {
+        try {
+            const { email, presenceAtTheEvent, phone } = request.body;
+            const { files } = request;
+
+            const guestRepository = new GuestsRepository();
+            const guestService = new SubscribeService(guestRepository, { email, presenceAtTheEvent, phone }, files);
+            const guestResponse = await guestService.execute();
+            return response.status(guestResponse.code).json(guestResponse);
+        } catch (error) {
             const err = handleErrors(error);
-            return response.status(error.statusCode ?? 500).json({data: err.description ?? err.message});
+            return response.status(error.statusCode ?? 500).json({ data: err.description ?? err.message });
         }
-    }else{
+    } else {
         response.setHeader('allow', 'PUT');
         response.status(405).end('Method not allowed');
     }
+});
+
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 }
 
 export default controllerSubscrible;

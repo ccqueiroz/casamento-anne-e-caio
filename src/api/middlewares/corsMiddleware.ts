b8a66608c4c1e91714d726bcf9d6bdbd4fb92480
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-30 00:34:44 -0300

    fix: add method get in access control allow methods

--- src/api/middlewares/corsMiddleware.ts
+++ src/api/middlewares/corsMiddleware.ts
@@ -3,7 +3,7 @@ import { NextApiRequestModels, NextApiResponseModels } from '../client/models/ne
 export default async function corsMiddleware(request: NextApiRequestModels, response: NextApiResponseModels, next: any) {
     response.setHeader('Access-Control-Allow-Credentials', "true");
     response.setHeader('Access-Control-Allow-Origin', '*');
-    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, PUT');
+    response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, PUT, GET');
     response.setHeader(
         'Access-Control-Allow-Headers',
         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'

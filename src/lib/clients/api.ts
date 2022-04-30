e60242d8752be8e355c4671a7df01aa1d8f7a34b
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-30 02:16:51 -0300

    includes access controlls in axios

--- src/lib/clients/api.ts
+++ src/lib/clients/api.ts
@@ -1,7 +1,12 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+    }
 });
 
 export { api };

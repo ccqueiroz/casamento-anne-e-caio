89773cc1a25207959c88090b2e32faa421da0d42
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-04-30 01:36:03 -0300

    exclude vercel.json and cody by next.config

--- src/api/middlewares/corsMiddleware.ts
+++ src/api/middlewares/corsMiddleware.ts
@@ -2,19 +2,9 @@ import { NextApiRequestModels, NextApiResponseModels } from '../client/models/ne
 import NextCors from 'nextjs-cors';
 
 export default async function corsMiddleware(request: NextApiRequestModels, response: NextApiResponseModels, next: any) {
-    // response.setHeader('Access-Control-Allow-Credentials', "true");
-    // response.setHeader('Access-Control-Allow-Origin', '*');
-    // response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    // response.setHeader(
-    //     'Access-Control-Allow-Headers',
-    //     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    // );
-    // if (request.method === 'OPTIONS') {
-    //    return response.status(200).end();
-    // }
    await NextCors(request, response, {
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-      origin: '*',
+      origin: 'https://casamentoanneecaio.com',
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
    });
     next();

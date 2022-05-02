4399390bb4658a68656ec22a91280f8672211a26
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-01 21:31:07 -0300

    refactor: includes nprogress in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,31 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import '../../styles/globals.css';
 import { colors } from '../../styles/themeChakra';
 import { Toaster } from 'react-hot-toast';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
+import '../../public/NprogressCSS/index.css';
 
 const theme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleStart = () => NProgress.start();
+        const handleStop = () => NProgress.done();
+
+        router.events.on('routeChangeStart', handleStart);
+        router.events.on('routeChangeComplete', handleStop);
+        router.events.on('routeChangeError', handleStop);
+
+        return () => {
+            router.events.off('routeChangeStart', handleStart)
+            router.events.off('routeChangeComplete', handleStop)
+            router.events.off('routeChangeError', handleStop)
+        }
+    }, [router]);
+
     return (
         <ChakraProvider theme={theme}>
             <Component {...pageProps} />

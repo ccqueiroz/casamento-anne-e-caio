0c70ac041f582df8994234adb14c81ba17e8affa
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-01 21:32:25 -0300

    refactor: includes timeout in axios

--- src/lib/clients/api.ts
+++ src/lib/clients/api.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    timeout: 1000 * 15, // Wait for 5 seconds
 });
 
 export { api };

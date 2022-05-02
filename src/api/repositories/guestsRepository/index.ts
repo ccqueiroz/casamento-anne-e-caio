bbc3563c669760ef4c3d4d094645275cb3888f51
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-02 13:45:03 -0300

    refactor: change type return getRepository Guests

--- src/api/repositories/guestsRepository/index.ts
+++ src/api/repositories/guestsRepository/index.ts
@@ -1,6 +1,6 @@
 import { Expr, query as q } from 'faunadb';
 import { fauna } from '../../../lib/clients/fauna';
-import { GuestsFaunaDB, GuestsModel } from '../../../data/model/Guests';
+import { GuestsModel } from '../../../data/model/Guests';
 import { ReturnFaunaDBInEXPR } from '../../../data/model/Api/FaunaDB';
 
 class GuestsRepository {
@@ -16,52 +16,52 @@ class GuestsRepository {
             )
         ).then((res) => res).catch(err => err);
         if (guests?.data) {
-            return guests?.data?.map((guest: ReturnFaunaDBInEXPR) => guest?.data)
+            return guests?.data?.map((guest: ReturnFaunaDBInEXPR<GuestsModel>) => guest?.data)
         }
         return [];
     }
 
     //getByPhone
-    public async queryGetGuestByPhone(phone: string): Promise<GuestsFaunaDB> {
-        const guest = await fauna.query<GuestsFaunaDB>(
+    public async queryGetGuestByPhone(phone: string): Promise<ReturnFaunaDBInEXPR<GuestsModel>> {
+        const guest = await fauna.query<ReturnFaunaDBInEXPR<GuestsModel>>(
             q.Get(q.Match(
                 q.Index('guests_by_phone'), phone
             ))
-        ).then((res: GuestsFaunaDB) => res).catch(err => err);
+        ).then((res: ReturnFaunaDBInEXPR<GuestsModel>) => res).catch(err => err);
         
         return guest;
     }
 
     //create
-    public async queryCreateGuest(dataBody: GuestsModel): Promise<GuestsFaunaDB> {
-        const guest = await fauna.query<GuestsFaunaDB>(
+    public async queryCreateGuest(dataBody: GuestsModel): Promise<ReturnFaunaDBInEXPR<GuestsModel>> {
+        const guest = await fauna.query<ReturnFaunaDBInEXPR<GuestsModel>>(
             q.Create(q.Collection(this.colletion), {data: {...dataBody}})
-        ).then((res: GuestsFaunaDB ) => res).catch(err => err);
+        ).then((res: ReturnFaunaDBInEXPR<GuestsModel> ) => res).catch(err => err);
         
         return guest;
     }
 
     //getRef 
-    public async queryGetRefGuest(phone: string): Promise<GuestsFaunaDB> {
-        const ref = await fauna.query<GuestsFaunaDB>(
+    public async queryGetRefGuest(phone: string): Promise<ReturnFaunaDBInEXPR<GuestsModel>> {
+        const ref = await fauna.query<ReturnFaunaDBInEXPR<GuestsModel>>(
             q.Select(
                 'ref', q.Get(q.Match(q.Index('guests_by_phone'), phone))
             )
-        ).then((res: GuestsFaunaDB ) => res).catch(err => err);
+        ).then((res: ReturnFaunaDBInEXPR<GuestsModel> ) => res).catch(err => err);
         
         return ref;
     }
 
     //update
-    public async queryUpdateGuest(dataBody: GuestsModel, guest_ref_id: string): Promise<GuestsFaunaDB> {
-        const guest = await fauna.query<GuestsFaunaDB>(
+    public async queryUpdateGuest(dataBody: GuestsModel, guest_ref_id: string): Promise<ReturnFaunaDBInEXPR<GuestsModel>> {
+        const guest = await fauna.query<ReturnFaunaDBInEXPR<GuestsModel>>(
             q.Update(
                 q.Ref(q.Collection(this.colletion), guest_ref_id),
                 {
                     data: {...dataBody}
                 }
             )
-        ).then((res: GuestsFaunaDB ) => res).catch(err => err);
+        ).then((res: ReturnFaunaDBInEXPR<GuestsModel> ) => res).catch(err => err);
         
         return guest;
     }

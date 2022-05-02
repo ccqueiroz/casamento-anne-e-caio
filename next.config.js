a06cc146a64f38dd67423aba2147368833d7b315
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-01 21:30:38 -0300

    refactor: includes nprogress and lottie

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    AWS_SECRETKEY: process.env.AWS_SECRETKEY,
+    AWS_ACCESSKEY: process.env.AWS_ACCESSKEY,
+    AWSREGION: process.env.AWSREGION,
+    AWSBUCKET: process.env.AWSBUCKET,
+    FAUNA_ADMIN_KEY: process.env.FAUNA_ADMIN_KEY,
+    NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
   async headers() {
     return [
       {
@@ -13,8 +22,7 @@ const nextConfig = {
         ]
       }
     ]
-  },
-
+  }
 }
 
 module.exports = nextConfig

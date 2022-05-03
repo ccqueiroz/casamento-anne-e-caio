432ab913db97cae7ddd75049df84a6e3adf98da7
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-03 13:05:52 -0300

    refactor: refactor modals and created singup dashboard

--- src/data/utils/convertFirstLetterEachWorldToUppercase.ts
+++ src/data/utils/convertFirstLetterEachWorldToUppercase.ts
@@ -1,9 +1,9 @@
-export const convertFirstLetterEachWorldToUppercase = (name: string): string => {
-    const arrName = name.split(" ");
-    arrName.forEach((word: string, index: number) => {
-        const firstLetter = word[0].toUpperCase();
-        const joinLetters = firstLetter + word.slice(1);
+export const convertFirstLetterEachWorldToUppercase = (name: string | undefined): string | undefined => {
+    const arrName = name?.split(" ");
+    arrName?.forEach((word: string, index: number) => {
+        const firstLetter = word[0]?.toUpperCase();
+        const joinLetters = firstLetter + word?.slice(1);
         arrName[index] = joinLetters;
     });
-    return arrName.join(" ");
+    return name ? arrName?.join(" ") : undefined;
 }

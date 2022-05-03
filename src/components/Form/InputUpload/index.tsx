235a4cdbad36f1d7ef03642c67e4b680628d097e
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-03 15:10:36 -0300

    refactor: includes message file type

--- src/components/Form/InputUpload/index.tsx
+++ src/components/Form/InputUpload/index.tsx
@@ -36,63 +36,76 @@ const InputUpload: React.FC<InputUploadProps> = ({
                 Para que possamos comemorar com segurança, solicitamos que nos envie o seu passaporte da vacina contra à Covid 19, com a confirmação da sua TERCEIRA DOSE.
             </Text>
             <Flex
-                mt="1.5rem"
-                width="100%"
-                flexDirection="column"
+              mt="1.5rem"
+              width="100%"
+              flexDirection="column"
             >
-                <Flex flexDirection={'column'} mb="2rem">
-                    {filesData?.map((file: FileWithPreview, index: number) => (
-                      <Flex
-                        key={index}
-                        justify="space-between"
-                        align="center"
-                        minW="26.56rem"
-                        w="26.56rem"
-                      >
-                        <Link href={URL.createObjectURL(file)} isExternal height="auto" display="flex" alignItems="center" mr="8px">
-                          <Icon as={RiAttachment2} fontSize="20" />
-                        </Link>
+              <Flex flexDirection={'column'} mb="2rem">
+                  {filesData?.map((file: FileWithPreview, index: number) => (
+                    <Flex
+                      key={index}
+                      justify="space-between"
+                      align="center"
+                      minW="26.56rem"
+                      w="26.56rem"
+                    >
+                      <Link href={URL.createObjectURL(file)} isExternal height="auto" display="flex" alignItems="center" mr="8px">
+                        <Icon as={RiAttachment2} fontSize="20" />
+                      </Link>
 
-                        <Text
-                          fontFamily="TimesRoman"
-                          textAlign="start"
-                          fontSize="15"
-                          fontWeight="regular"
-                          w="100%"
-                        >
-                          {file?.name?.substr(0, 40)}
-                        </Text>
-                        <Icon
-                          as={MdClose}
-                          onClick={() => deleteAttachment(file?.name)}
-                          fontSize="20"
-                          color="#FF4D4D"
-                          _hover={{
-                            cursor: 'pointer',
-                          }}
-                        />
-                      </Flex>
-                    ))}
-                </Flex>
-                <Flex width="100%" justifyContent="center">
-                  <Button
-                    fontFamily="TimesRoman"
-                    disabled={filesData.length > 0}
-                    margin="0 auto"
-                    color="text.tertiary"
-                    padding="1rem"
-                    background="linear-gradient(45deg, #aadae9, #d6eef5)"
-                    transition="background 300ms easy-in-out"
-                    boxShadow="1px 2px 9px 2px rgba(74, 97, 97, 0.5)"
-                    leftIcon={<RiUpload2Fill fontSize={22} color="text.tertiary"/>}
-                    _hover={{
-                        backgroundImage: "linear-gradient(45deg, #93c2c2, #93c2c2, #aadae9, #d6eef5, #93c2c2)"
-                    }}
-                    onClick={openModal}
-                  >
-                    Selecione o arquivo
-                  </Button>
-                </Flex>
+                      <Text
+                        fontFamily="TimesRoman"
+                        textAlign="start"
+                        fontSize="15"
+                        fontWeight="regular"
+                        w="100%"
+                      >
+                        {file?.name?.substr(0, 40)}
+                      </Text>
+                      <Icon
+                        as={MdClose}
+                        onClick={() => deleteAttachment(file?.name)}
+                        fontSize="20"
+                        color="#FF4D4D"
+                        _hover={{
+                          cursor: 'pointer',
+                        }}
+                      />
+                    </Flex>
+                  ))}
+              </Flex>
+              <Flex width="100%" justifyContent="center">
+                <Button
+                  fontFamily="TimesRoman"
+                  disabled={filesData.length > 0}
+                  margin="0 auto"
+                  color="text.tertiary"
+                  padding="1rem"
+                  background="linear-gradient(45deg, #aadae9, #d6eef5)"
+                  transition="background 300ms easy-in-out"
+                  boxShadow="1px 2px 9px 2px rgba(74, 97, 97, 0.5)"
+                  leftIcon={<RiUpload2Fill fontSize={22} color="text.tertiary"/>}
+                  _hover={{
+                      backgroundImage: "linear-gradient(45deg, #93c2c2, #93c2c2, #aadae9, #d6eef5, #93c2c2)"
+                  }}
+                  onClick={openModal}
+                >
+                  Selecione o arquivo
+                </Button>
+              </Flex>
+              <Text
+                marginTop="1rem"
+                fontFamily="TimesRoman"
+                fontWeight="bold"
+                letterSpacing="0.2rem"
+                color="#0c6a6b"
+                fontSize={{ base: "0.75 rem", md: "0.825rem" }}
+                textAlign="center"
+                wordBreak="break-word"
+                style={{textIndent: 0}}
+              >
+                  * o arquivo enviado deverá ser do tipo PDF.
+              </Text>
             </Flex>
         </Flex>
     );

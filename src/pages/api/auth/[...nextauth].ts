47d8f6ac4669f4321fe4e45fadfde53faaa03d78
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-10 02:29:08 -0300

    teste de bug com variaveis de ambiente

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,8 +6,8 @@ export default NextAuth({
     secret: process.env.SECRET_KEY,
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+            clientId: String(process.env.GOOGLE_CLIENT_ID),
+            clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
             authorization: {
                 params: {
                     prompt: "consent",
@@ -19,6 +19,8 @@ export default NextAuth({
     ],
     callbacks: {
         async signIn({ account, profile, ...params }) {
+            console.log('process.env.GOOGLE_CLIENT_ID', process.env.GOOGLE_CLIENT_ID)
+            console.log('process.env.GOOGLE_CLIENT_ID', process.env.GOOGLE_CLIENT_SECRET)
             const userRepository = new UserAdminRepository();
             if (account.provider === "google") {
                 if (profile?.email_verified && profile?.email) {

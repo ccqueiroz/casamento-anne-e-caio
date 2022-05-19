7b6acd37ff7648deaeee6ada393001e2380a2ffe
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-19 01:39:53 -0300

    feat: created controllerCreatedGuest

---
+++ src/pages/api/dashboard/create-guest.ts
@@ -0,0 +1,40 @@
+import { NextApiRequestModels, NextApiResponseModels } from '../../../api/client/models/nextHTTPModels';
+import { handleErrors } from '../../../api/errors/handleErrors';
+import { GuestsRepository } from '../../../api/repositories/guestsRepository';
+import nextConnect from 'next-connect';
+import parseMultiPartyForm from '../../../api/middlewares/multipartyFormMiddleware';
+import corsMiddleware from '../../../api/middlewares/corsMiddleware';
+import { CreateGuestService } from '../../../api/services/CreateGuestService';
+
+const controllerCreatedGuest = nextConnect();
+controllerCreatedGuest
+    .use(corsMiddleware)
+    .use(parseMultiPartyForm)
+    .post(async (request: NextApiRequestModels, response: NextApiResponseModels) => {
+    if (request.method === 'POST') {
+        try {
+            const { email, presenceAtTheEvent, phone } = request.body;
+            const { files } = request;
+            const guestRepository = new GuestsRepository();
+            const guestService = new CreateGuestService(guestRepository, { email, presenceAtTheEvent, phone }, files);
+            const guestResponse = await guestService.execute();
+            return response.status(guestResponse.code).json(guestResponse);
+        } catch (error) {
+            const err = handleErrors(error);
+            console.error(err)
+            return response.status(err.statusCode).json({ data: err.message });
+        }
+    } else {
+        response.setHeader('allow', 'POST');
+        response.status(405).end('Method not allowed');
+    }
+});
+
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default controllerCreatedGuest;

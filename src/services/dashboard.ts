c758eca0d47bdfc5061e9a75fc7c940eabf54c58
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-19 02:53:42 -0300

    refactor: change types guestList

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,11 +1,12 @@
 import { HandleMessageResponse } from "../data/model/Api/HandleMessageModel";
+import { GuestsModel } from "../data/model/Guests";
 import { api } from "../lib/clients/api";
 
 
 const API_PATH = '/dashboard';
 
 const guestsList = async () => {
-    const { data } = await api.get<HandleMessageResponse>(`${API_PATH}/guest-list`);
+    const { data } = await api.get<HandleMessageResponse<Array<GuestsModel>>>(`${API_PATH}/guest-list`);
     return data;
 }
 

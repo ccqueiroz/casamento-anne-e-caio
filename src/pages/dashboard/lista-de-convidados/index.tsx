5a0bfa8964f4ab168a109f5247bab98b5b6f0b52
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2022-05-18 22:48:24 -0300

    refactor: includes statistic button

--- src/pages/dashboard/lista-de-convidados/index.tsx
+++ src/pages/dashboard/lista-de-convidados/index.tsx
@@ -10,6 +10,7 @@ import Dashboard from '../Layout';
 import { TextComponent } from '../../../components/TextComponent';
 import { CreateOrEditGuest } from '../../../components/Overlay/CreateOrEditGuest';
 import { InscriptionType } from '../../../data/enums/InscriptionType';
+import { ModalStatisticGuests } from '../../../components/Overlay/ModalStatisticGuestes';
 
 interface GuestListProps extends SessionProps<Array<GuestsModel>>{
     guestsList: Array<GuestsModel>
@@ -17,6 +18,7 @@ interface GuestListProps extends SessionProps<Array<GuestsModel>>{
 
 const GuestList: React.FC<GuestListProps> = ({user, guestsList }) => {
     const { onOpen: onOpenModalCreateOrEdit, onClose: onCloseModalCreateOrEdit,...propsModalCreateOrEdit } = useDisclosure();
+    const { onOpen: onOpenModalStatisticGuests, onClose: onCloseModalStatisticGuests,...propsModalStatisticGuests } = useDisclosure();
     const [guest, setGuest] = useState<GuestsModel | undefined>(undefined);
     const [inscriptionType, setInscriptionType] = useState<InscriptionType | undefined>(undefined);
 
@@ -34,11 +36,6 @@ const GuestList: React.FC<GuestListProps> = ({user, guestsList }) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const numberGuestConfirmed = useMemo(() => {
-        const data = guestsList?.filter((guest) => guest?.presenceAtTheEvent === 'Y');
-        return data?.length;
-    }, [guestsList]);
-
     return (
         <Dashboard user={user}>
             <Box width="100%" height="100%" id="content-guest-list" overflow="hidden">
@@ -51,29 +48,52 @@ const GuestList: React.FC<GuestListProps> = ({user, guestsList }) => {
                     <TextComponent text="Lista de Convidados" textIndent={0} textAlign="center" fontSize="2rem" marginTop="2rem" color="text.secondary" />
                     <Box
                         width="100%"
-                        maxWidth="1380px"
-                        height="3rem"
+                        maxWidth="1400px"
+                        height={{ base: '7rem', md: '3rem'}}
                         margin="1.25rem auto 0"
+                        padding="0 10px"
                     >
-                        <Box height="100%" display="flex" alignItems="flex-end">
+                        <Box height="100%" display="flex" alignItems={{base: 'center', md: "flex-end"}} flexDirection={{base: 'column-reverse', md: 'row'}}>
                             <TextComponent text={`${guestsList?.length} convidados`} textIndent={0} textAlign="left" fontSize="1rem" color="text.secondary" />
-                            <TextComponent text={` | `} textIndent={0} textAlign="left" fontSize="1rem" color="text.secondary" margin="0 12px"/>
-                            <TextComponent text={`${numberGuestConfirmed} convidados confirmados`} textIndent={0} textAlign="left" fontSize="1rem" color="text.secondary" />
                             <Flex
                                 alignItems="center"
                                 margin="0 auto"
                                 flex={1}
                                 justifyContent="flex-end"
+                                flexDirection={{base: 'column', md: 'row'}}
                             >
                                 <Button
                                     width="auto"
                                     height="30px"
                                     marginBottom="8px"
-                                    padding="2%"
+                                    marginRight="1rem"
+                                    padding={{base: "3% 15%", md: "1%"}}
                                     fontWeight="bold"
-                                    letterSpacing="0.2rem"
+                                    letterSpacing="0.1rem"
                                     color="#0c6a6b"
-                                    fontSize={{ base: "0.825rem", md: "1rem" }}
+                                    fontSize={{ base: "0.725rem", md: "0.8rem" }}
+                                    background="linear-gradient(45deg, #aadae9, #d6eef5)"
+                                    transition="background 300ms easy-in-out"
+                                    boxShadow="1px 2px 9px 2px rgba(74, 97, 97, 0.5)"
+                                    title="Adicionar um novo convidado"
+                                    aria-describedby="Adicionar um novo convidado"
+                                    aria-labelledby="Adicionar um novo convidado"
+                                    onClick={() => onOpenModalStatisticGuests()}
+                                    _hover={{
+                                        backgroundImage: "linear-gradient(45deg, #93c2c2, #93c2c2, #aadae9, #d6eef5, #93c2c2)"
+                                    }}
+                                >
+                                    Estatísticas             
+                                </Button>
+                                <Button
+                                    width="auto"
+                                    height="30px"
+                                    marginBottom="8px"
+                                    padding={{base: "3% 15%", md: "1%"}}
+                                    fontWeight="bold"
+                                    letterSpacing="0.1rem"
+                                    color="#0c6a6b"
+                                    fontSize={{ base: "0.725rem", md: "0.8rem" }}
                                     background="linear-gradient(45deg, #aadae9, #d6eef5)"
                                     transition="background 300ms easy-in-out"
                                     boxShadow="1px 2px 9px 2px rgba(74, 97, 97, 0.5)"
@@ -88,6 +108,7 @@ const GuestList: React.FC<GuestListProps> = ({user, guestsList }) => {
                                     Adicionar Convidado             
                                 </Button>
                             </Flex>
+                            
                         </Box>
                     </Box>
                     <Box
@@ -116,6 +137,11 @@ const GuestList: React.FC<GuestListProps> = ({user, guestsList }) => {
                     </Box>
 
                 </Box>
+                <ModalStatisticGuests
+                    guestsList={guestsList}
+                    onClose={onCloseModalStatisticGuests}
+                    {...propsModalStatisticGuests}
+                />
                 <CreateOrEditGuest
                     inscriptionType={inscriptionType as InscriptionType}
                     onClose={onCloseModalCreateOrEditOverride}
